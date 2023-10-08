53a4359ce81008559d74603e98f10b5cd3850bbe
Pea Masamba <96761006+NadifMasamba@users.noreply.github.com>
2023-10-08 15:31:34 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -51,13 +51,13 @@ async def stats(_, message):
         'Mega'     : config_dict.get('MEGA_LIMIT',     '∞'),
         'User task': config_dict.get('USER_MAX_TASKS', '∞')}
     system_info = f'<b>{quote}</b>\n\n'\
-        f'<code>• Bot uptime :</code> {currentTime}\n'\
-        f'<code>• Sys uptime :</code> {osUptime}\n'\
-        f'<code>• CPU usage  :</code> {cpuUsage}%\n'\
-        f'<code>• RAM usage  :</code> {memory.percent}%\n'\
-        f'<code>• Disk usage :</code> {disk}%\n'\
-        f'<code>• Free space :</code> {get_readable_file_size(free)}\n'\
-        f'<code>• Total space:</code> {get_readable_file_size(total)}\n\n'
+        f'<code>Bot uptime :</code> {currentTime}\n'\
+        f'<code>Sys uptime :</code> {osUptime}\n'\
+        f'<code>CPU usage  :</code> {cpuUsage}%\n'\
+        f'<code>RAM usage  :</code> {memory.percent}%\n'\
+        f'<code>Disk usage :</code> {disk}%\n'\
+        f'<code>Free space :</code> {get_readable_file_size(free)}\n'\
+        f'<code>Total space:</code> {get_readable_file_size(total)}\n\n'
             
     limitations = f'<b>LIMITATIONS</b>\n\n'
     
