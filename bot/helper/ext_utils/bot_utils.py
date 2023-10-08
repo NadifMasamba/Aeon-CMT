f19ec3af65462a042282fda55ba4bc00df0ee702
Pea Masamba <96761006+NadifMasamba@users.noreply.github.com>
2023-10-08 19:34:39 +0700

    Fix

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -174,7 +174,7 @@ def get_readable_message():
         msg += f"\n</b><code>{download.status()}</code>"
         if download.status() not in [MirrorStatus.STATUS_SPLITTING, MirrorStatus.STATUS_SEEDING]:
             msg += f"\n<b><code>{progress_bar(download.progress())} » <code>{download.progress()}</code></b>"
-            msg += f"\n{download.processed_bytes()} of {download.size()}"
+            msg += f"\n<b>Done: <code>{download.processed_bytes()} of {download.size()}</b></code>"
             msg += f"\n<b>Speed: <code>{download.speed()}</b></code>"
             msg += f"\n<b>By: <code>{source(download)}</b></code>"
             msg += f'\n<b>ETA: <code>{download.eta()}</b></code>'
